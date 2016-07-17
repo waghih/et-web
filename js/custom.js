2cba4c0b295df8faf7024a8f06009bc1fc549d04
waghih <waghih.co@gmail.com>
2016-07-17 20:54:12 +0800

    initial project

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+(function($){
+
+	$(document).ready(function() {
+
+		/* ---------------------------------------------- /*
+		 * Smooth scroll / Scroll To Top
+		/* ---------------------------------------------- */
+
+		$('a[href*=#]').bind("click", function(e){
+           
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('.scroll-up').fadeIn();
+			} else {
+				$('.scroll-up').fadeOut();
+			}
+		});
+
+		/* ---------------------------------------------- /*
+		 * Navbar
+		/* ---------------------------------------------- */
+
+		$('.header').sticky({
+			topSpacing: 0
+		});
+
+		$('body').scrollspy({
+			target: '.navbar-custom',
+			offset: 70
+		})
+        
+		/* ---------------------------------------------- /*
+		 * Home BG
+		/* ---------------------------------------------- */
+
+		$(".screen-height").height($(window).height());
+
+		$(window).resize(function(){
+			$(".screen-height").height($(window).height());
+		});
+
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+			$('#home').css({'background-attachment': 'scroll'});
+		} else {
+			$('#home').parallax('50%', 0.1);
+		}
+
+	});
+
+})(jQuery);
